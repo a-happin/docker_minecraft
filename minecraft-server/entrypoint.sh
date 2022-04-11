2ac3c7cfd2cc4e1306918b98ac07ba2da12d122a
a_happin <10003601+a@users.noreply.github.com>
2022-04-11 16:42:22 +0900

    Add upgrade

--- minecraft-server/entrypoint.sh
+++ minecraft-server/entrypoint.sh
@@ -3,5 +3,6 @@
 useradd -m -u ${DOCKER_UID:-1000} ${DOCKER_USER} && groupmod -g ${DOCKER_GID:-1000} ${DOCKER_USER}
 export HOME=/home/${DOCKER_USER}
 
-[ -z "$*" ] && set -- java -Xms${JVM_MEM:-1G} -Xmx${JVM_MEM:-1G} -jar /opt/minecraft/${MINECRAFT_VERSION}.server.jar nogui
+[ -z "$*" ] && set -- java -Xms${JVM_MEM:-1G} -Xmx${JVM_MEM:-1G} -jar /opt/minecraft/${MINECRAFT_VERSION}.server.jar --nogui
+[ "$*" = "upgrade" ] && set -- java -Xms${JVM_MEM:-1G} -Xmx${JVM_MEM:-1G} -jar /opt/minecraft/${MINECRAFT_VERSION}.server.jar --forceUpgrade --eraseCache --nogui
 cd /mnt/minecraft && exec /usr/sbin/gosu ${DOCKER_USER} "$@"
